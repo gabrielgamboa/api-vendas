26b24af9b891b1b000cc60297c148b62180d7065
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-10-26 19:38:23 -0300

    feat: repositorio de produtos

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @CreateDateColumn()
+  deletedAt: Date;
+}
