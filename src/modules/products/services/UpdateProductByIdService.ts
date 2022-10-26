aac1e5dc1ab2e87fb3dfaa41f0daa824c8bf02f0
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-10-26 20:02:22 -0300

    feat: update de produto

---
+++ src/modules/products/services/UpdateProductByIdService.ts
@@ -0,0 +1,45 @@
+import { ApplicationError } from '@shared/errors/ApplicationError';
+import { getCustomRepository } from 'typeorm';
+import { Product } from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class UpdateProductById {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!product) throw new ApplicationError('Product not found');
+
+    const productsExistsWithSameName = await productsRepository.findByName(
+      name,
+    );
+
+    if (productsExistsWithSameName)
+      throw new ApplicationError('There is already one product whit this name');
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
