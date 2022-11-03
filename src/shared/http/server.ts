52665878a91e4c9e6eba97404fd2f0f5f59ae02e
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-11-03 12:02:09 -0300

    feat: usando celebrate para validações de dados

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import 'reflect-metadata';
+import { errors } from 'celebrate';
+import 'express-async-errors';
 import '@shared/typeorm';
 
 import { routes } from './routes';
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors()); //earlier import than custom error middleware because if joi throws an error this middleware will catch it
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ApplicationError) {
